bf82c91b3f5eb1001c5bd5bc6c59e0dc9255ed9e
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-12-04 21:18:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
+    
 SESSION_ID: process.env.SESSION_ID || "rn41BDKQ#WUsMskXV5TF7M3BIY58cQdtpxOxJ7RvFaOG0rpW8rOw",
 PREFIX: process.env.PREFIX || ".",
+MODE: process.env.MODE || "public",
+
 }
