e0703d8ef5ec16a4163b25f76de3914e4dfa301a
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-12-04 20:57:25 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+const prefix = config.PREFIX
 
 const ownerNumber = ['94789613424']
 
