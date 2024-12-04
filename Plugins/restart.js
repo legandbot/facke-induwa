b7e6d3f02f70b9dada6cef0425ebe57845819245
Sadarulk <126694422+Sadarulk@users.noreply.github.com>
2024-12-04 21:00:46 +0530

    Update restart.js

--- Plugins/restart.js
+++ Plugins/restart.js
@@ -10,10 +10,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+    
 const {exec} = require("child_process")
-reply("restarting...")
+reply("*Bot restarting...*")
 await sleep(1500)
 exec("pm2 restart all")
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
