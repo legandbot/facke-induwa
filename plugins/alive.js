6aca5fbb476f29bd67066f44cb2128f1188ee05f
legandbot <156310542+legandbot@users.noreply.github.com>
2024-12-05 13:34:52 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     
-await conn.sendMessage(from,{image: {url: `https://i.ibb.co/jyVLMdD/infinitywabot.jpg`},caption: `Hello ${pushname}, I am alive now !`},{quoted: mek})
+await conn.sendMessage(from,{image: {url: `https://i.ibb.co/jyVLMdD/infinitywabot.jpg`},caption: `Hello ${pushname}, hello iam created by induwara🌝❤️‍🔥 !`},{quoted: mek})
     
 }catch(e){
 console.log(e)
